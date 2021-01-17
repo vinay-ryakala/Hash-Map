95f378b7f95e19a8518a65b5c8957c7450330d90
vinay-ryakala <vinayrakyala@gmail.com>
2021-01-17 21:43:49 +0530

    ADDED:find frequency of word in a paragraph

---
+++ src/main/java/com/bridgelabz/LinkedHashMap.java
@@ -0,0 +1,54 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+
+public class LinkedHashMap<K, V> {
+   private final int numBuckets;
+   ArrayList<LinkedList<K>> myBucketArray;
+
+   public LinkedHashMap() {
+      this.numBuckets = 10;
+      this.myBucketArray = new ArrayList<>(numBuckets);
+      for (int i = 0; i < numBuckets; i++)
+         this.myBucketArray.add(null);
+   }
+
+   public V get(K key) {
+      int index = this.getBucketIndex(key);
+      LinkedList<K> linkedList = this.myBucketArray.get(index);
+      if (linkedList == null)
+         return null;
+      MapNode<K, V> mapNode = (MapNode<K, V>) linkedList.searchNode(key);
+      return (mapNode == null) ? null : mapNode.getValue();
+   }
+
+   private int getBucketIndex(K key) {
+      int hashCode = Math.abs(key.hashCode());
+      int index = hashCode % numBuckets;
+      System.out.println("Key: " + key + " hashcode: " + hashCode + " index: " + index);
+      return index;
+   }
+
+   public void add(K key, V value) {
+      int index = this.getBucketIndex(key);
+      LinkedList<K> linkedList = this.myBucketArray.get(index);
+      if (linkedList == null) {
+         linkedList = new LinkedList<>();
+         this.myBucketArray.set(index, linkedList);
+      }
+      MapNode<K, V> mapNode = (MapNode<K, V>) linkedList.searchNode(key);
+      if (mapNode == null) {
+         mapNode = new MapNode<>(key, value);
+         linkedList.append(mapNode);
+      } else {
+         mapNode.setValue(value);
+      }
+   }
+
+   @Override
+   public String toString() {
+      return "LinkedHashMap List { " + myBucketArray + " }";
+
+   }
+
+}
--- src/test/java/com/bridgelabz/HashMapTest.java
+++ src/test/java/com/bridgelabz/HashMapTest.java
@@ -20,4 +20,21 @@ public class HashMapTest{
       System.out.println(hashMap);
       Assert.assertEquals(2, frequency);
    }
+   @Test
+   public void givenParagraphReturnFrequency() {
+      String sentence = "Paranoids are not paranoid because "
+              + "they are paranoid but because they keep putting themselves deliberately into "
+              + "paranoid avoidable situations";
+      LinkedHashMap<String, Integer> linkedHashMap = new LinkedHashMap<>();
+      String[] words = sentence.toLowerCase().split(" ");
+      for(String word : words) {
+         Integer value = linkedHashMap.get(word);
+         if(value == null) value = 1;
+         else value = value + 1;
+         linkedHashMap.add(word, value);
+      }
+      int frequency = linkedHashMap.get("paranoid");
+      System.out.println(linkedHashMap);
+      Assert.assertEquals(3, frequency);
+   }
 }
